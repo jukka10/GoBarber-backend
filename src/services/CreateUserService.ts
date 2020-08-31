340360cad06b7669bb977d28670ba6845c7c129d
jukka <jukka@iMac-de-jukka.home>
2020-08-31 10:27:02 -0300

    encrypting password

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 
+import { hash } from 'bcryptjs';
 import User from '../models/User';
 
 interface Request {
@@ -20,13 +21,17 @@ export default class CreateUserService {
       throw new Error('Email address already used');
     }
 
+    const hashedPassword = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await usersRepository.save(user);
+
+    delete user.password;
 
     return user;
   }
